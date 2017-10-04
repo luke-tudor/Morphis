2d46a0aa8af7a9413d3d94b497ba799aa63386d2
Kieran Folwell <kfol123@aucklanduni.ac.nz>
2017-10-05 11:25:08 +1300

    Added a trigger script for lectern

---
+++ Assets/Scripts/UserIsClose.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserIsClose : MonoBehaviour
+{
+
+    public GameObject Text;
+    private bool isShowing;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Text.GetComponent<CanvasGroup>().alpha = 1f;
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        Text.GetComponent<CanvasGroup>().alpha = 0f;
+    }
+}
