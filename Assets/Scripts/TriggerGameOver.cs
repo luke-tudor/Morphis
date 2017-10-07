7c21a68ef9282f4e318515d9b9354c6e60934b77
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-10-07 14:43:55 +1300

    Create script to trigger game over screen

---
+++ Assets/Scripts/TriggerGameOver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class TriggerGameOver : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("WHAT");
+        Debug.Log(other.tag);
+        if (other.CompareTag("ExitLevelTrigger"))
+        {
+
+            SceneManager.LoadScene("EndGame");
+
+        }
+    }
+}
+
