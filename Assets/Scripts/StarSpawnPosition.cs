8545d970de897647f3c6243efd1da7e62541b791
Max Griffith <mgriffith37@gmail.com>
2017-10-09 23:14:02 +1300

    Added code comments through the star scripts

--- Assets/Scripts/StarSpawnPosition.cs
+++ Assets/Scripts/StarSpawnPosition.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class StarSpawnPosition : MonoBehaviour {
 	public GameObject star;
 
+	//Method called when this star spawn position is chosen to spawn a star
 	void Spawn(){
+		//Spawns a star object in the same position as this spawn position
 		star = GameObject.FindGameObjectWithTag ("Star");
 		Instantiate (star, transform.position, transform.rotation);
 	}
