d3d9d58fe990cef4f8157f378bf41752aa524969
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-10-09 13:27:47 +1300

    Display number of stars collected at end of game

---
+++ Assets/Scripts/DisplayStarsCollected.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayStarsCollected : MonoBehaviour {
+
+    private Text instruction;
+    void Start()
+    {
+        instruction = GetComponent<Text>();
+        instruction.text = "You collected " + new StarSpawner().GetStarCount() + " stars!";
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
