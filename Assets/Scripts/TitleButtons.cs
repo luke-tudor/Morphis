c82e834e92c28c06922ec34053459b634b32859f
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-10-09 22:31:27 +1300

    Comment scripts

--- Assets/Scripts/TitleButtons.cs
+++ Assets/Scripts/TitleButtons.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
+
+/*
+ * Class that adds methods that are called when the respective buttons are clicked
+ */
 public class TitleButtons : MonoBehaviour
 {
 
+    // Load the prototype level
     public void Play()
     {
         SceneManager.LoadScene("PrototypeLevel2");
     }
 
+    // Quit the game
     public void Quit()
     {
         Application.Quit();
