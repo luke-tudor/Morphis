4f7d6bb14cf581b2f114a161582b2feedd61acfd
Charles Carey <ccar845@aucklanduni.ac.nz>
2017-10-15 12:03:01 +1300

    Fix collision bugs for blocks moving in the y axis

--- Assets/Scripts/ShapeChange.cs
+++ Assets/Scripts/ShapeChange.cs
@@ -39,7 +39,9 @@ public class ShapeChange : MonoBehaviour
     private IDictionary<Renderer, Color> _defaultMatColors;
     private Renderer[] _renderers;
     private bool _collisionDetected;
-    private bool _grownThisUpdate;
+	private bool _grownThisUpdate;
+	private bool _growsDown;
+	private bool _growsUp;
 
     // Use this for initialization
     void Start()
@@ -48,6 +50,8 @@ public class ShapeChange : MonoBehaviour
         _desiredScale = (int)transform.localScale.y;
         _collisionDetected = false;
         _grownThisUpdate = false;
+		_growsDown = transform.rotation.eulerAngles.y == 180 && transform.rotation.eulerAngles.z == 180;
+		_growsUp = transform.rotation.eulerAngles.x == 0 && transform.rotation.eulerAngles.z == 0 && transform.rotation.eulerAngles.y == 0;
     }
 
     void OnTriggerEnter(Collider collision)
@@ -113,19 +117,40 @@ public class ShapeChange : MonoBehaviour
     /// <summary>
     /// Grow the block
     /// </summary>
-    public void Grow()
-    {
-        if (!Extrudable || _collisionDetected)
-            return;
+	public void Grow()
+	{
+		if (!Extrudable || _collisionDetected)
+			return;
 
-        _grownThisUpdate = true;
-        _desiredScale = Mathf.CeilToInt(transform.localScale.y + 0.001f);
+		if (_growsDown) {
+			GameObject person = GameObject.Find("Player");
+			double heightDifference = this.gameObject.transform.position.y - GetComponent<Collider> ().bounds.size.y - person.transform.position.y;
+			if (heightDifference <= 3.2 && isInBoundsOfObject(person)) {
+				return;
+			}
+		}
 
-        if (!_grindingNoise.isPlaying)
-        {
-            _grindingNoise.Play();
-        }
-    }
+		if (_growsUp) {
+			RaycastHit hit;
+			Vector3 origin = transform.position;
+			origin.y += GetComponent<Collider> ().bounds.size.y;
+			if(Physics.Raycast (GetComponent<Collider> ().bounds.center, Vector3.up, out hit)) {
+				GameObject person = GameObject.Find("Player");
+				double heightDifference = hit.collider.gameObject.transform.position.y - person.transform.position.y;
+				if (heightDifference <= 3 && isInBoundsOfObject(person)) {
+					return;
+				}
+			}
+		}
+
+		_grownThisUpdate = true;
+		_desiredScale = Mathf.CeilToInt(transform.localScale.y + 0.001f);
+
+		if (!_grindingNoise.isPlaying)
+		{
+			_grindingNoise.Play();
+		}
+	}
 
     /// <summary>
     /// Shrink the block
@@ -148,4 +173,19 @@ public class ShapeChange : MonoBehaviour
     {
         _desiredScale = desiredScale;
     }
+
+	private bool isInBoundsOfObject (GameObject obj) {
+		double midZ = GetComponent<Collider> ().bounds.center.z;
+		double lowZ = midZ - GetComponent<Collider> ().bounds.size.z / 2;
+		double highZ = midZ + GetComponent<Collider> ().bounds.size.z / 2;
+
+		double midX = GetComponent<Collider> ().bounds.center.x;
+		double lowX = midX - GetComponent<Collider> ().bounds.size.x / 2;
+		double highX = midX + GetComponent<Collider> ().bounds.size.x / 2;
+
+		bool playerIsInXBounds = lowX <= obj.transform.position.x && obj.transform.position.x <= highX;
+		bool playerIsInZBounds = lowZ <= obj.transform.position.z && obj.transform.position.z <= highZ;
+
+		return playerIsInXBounds && playerIsInZBounds;
+	}
 }
