5817f91ed6cd4f121d9466478175c1afb177f33c
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-10-21 12:26:52 +1300

    Update scene change script to allow variable next level

---
+++ Assets/Scripts/TriggerSceneChange.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+/*
+ * Class has methods to trigger another scene
+ */
+public class TriggerSceneChange : MonoBehaviour
+{
+    public string NextLevel;
+    // If the collision is with the player load the next scene
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hello");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("hello1");
+            SceneManager.LoadScene(NextLevel);
+        }
+    }
+}
+
