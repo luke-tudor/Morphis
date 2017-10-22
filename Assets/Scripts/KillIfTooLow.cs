024309da2a3907c9b996e5e84e63fe1b2930a4fd
luke-tudor <ltud719@aucklanduni.ac.nz>
2017-10-23 12:40:46 +1300

    added a script that kills the player if they fall off the map

---
+++ Assets/Scripts/KillIfTooLow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class KillIfTooLow : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = GetComponent<Transform>().position;	
+        if (position.y < -200)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
